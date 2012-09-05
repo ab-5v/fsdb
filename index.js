f1d372ced146f3fe513999957731515a6b95f613
Artur Burtsev <artjock@gmail.com>
2012-09-05 19:45:02 +0400

    public interface

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 var fsdb = require('./lib/fsdb');
 fsdb.version = require('./package.json').version;
 
-module.exports = fsdb;
+module.exports = {
+    open: function(path, names, callback) {
+        var cb = callback || function() {};
+        var db = new fsdb(path, names, callback);
+
+        db.open(function(err, db) {
+            if (err) { return cb(err); }
+
+            cb(null, db.collections);
+        });
+
+        if (!callback) {
+            return db.collections;
+        }
+    }
+};
